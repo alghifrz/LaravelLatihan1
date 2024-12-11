85cc8badae117bfa4b7ce0687e4a91b4ad353577
alghifrz <alghifarirasyidzola@gmail.com>
2024-12-11 10:14:44 +0700

    commit 2

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 
 Route::get('/signin', function () {
-   return 'Signin Page'; 
+   return view('signin'); 
 });
 
 Route::get('/signup', function () {
-    return 'Signup Page';
+    return view('signup');
 });
 
 Route::get('/home', function () {
